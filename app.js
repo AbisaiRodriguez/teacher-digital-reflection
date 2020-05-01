53df500f07b8266fce8a64d12b3531153519f7b6
Abisai Rodriguez <abisai.rodriguez13@gmail.com>
2020-05-01 11:54:27 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express"),
+    app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("home")
+});
+
+app.get("/cstpGrowthDevelopment", (req, res) => {
+    res.render("cstpGrowthDevelopment")
+});
+
+app.get("/impactOnStudentSuccess", (req, res) => {
+    res.render("impactOnStudentSuccess")
+});
+
+//app.listen(3000);
+app.listen(process.env.PORT, process.env.IP);
